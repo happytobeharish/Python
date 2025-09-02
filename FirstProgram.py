65ccef43c8d801c134a4ee5d05731e17b50bbc85
happytobeharish <happytobeharish@gmail.com>
2025-09-02 17:22:47 +0530

    Python Day 2

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,3 +1,27 @@
-a=int(input("Enter first value :"))
-b=int(input("Enter second value :"))
-print(a>=b)
+#a=int(input("Enter first value :"))
+#b=int(input("Enter second value :"))
+#print(a>=b)
+
+#name = input("(str)Enter Your Name :")
+#print("Length of your name is :", len(name))
+
+
+#str = "hi i am $ symbol $99.99 102$ 100k usd$" 
+#print(str.count("$"))
+
+
+
+marks = int(input(" Enter The Marks :"))
+
+if (marks  >= 90) :
+    grade ="A"
+elif (marks >=80 and marks <90):
+    grade ="B"
+elif (marks >=70 and marks <80):
+    grade ="C"
+elif (marks >=60 and marks <70):
+    grade ="D"
+else : 
+    grade="Fail"            
+
+print("grade of the student ->", grade)
